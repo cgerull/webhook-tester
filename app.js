fa6f954aeb3a1ff54a0f489da13b7839b78bfbb0
Claus Gerull <claus.gerull@atos.net>
2018-04-16 12:05:43 +0200

    Build GitHub webhook testserver.

--- app.js
+++ app.js
@@ -1,13 +1,32 @@
+// Simple echo for GitHub webhook testing
+var http = require('http');
+const config = require('./config/config');
+var createHandler = require('github-webhook-handler');
+var handler = createHandler({ path: '/webhook', secret: 'myhashsecret' });
 
+http.createServer(function (req, res) {
+  handler(req, res, function (err) {
+    res.statusCode = 404;
+    res.end('no such location');
+  })
+}).listen(config.port, () => {
+   console.log('Express server listening on port ' + config.port);
+ });
 
-const express = require('express');
-const config = require('./config/config');;
+handler.on('error', function (err) {
+  console.error('Error:', err.message);
+})
 
-const app = express();
+handler.on('push', function (event) {
+  console.log('Received a push event for %s to %s',
+    event.payload.repository.name,
+    event.payload.ref)
+})
 
-module.exports = require('./config/express')(app, config);
-
-app.listen(config.port, () => {
-  console.log('Express server listening on port ' + config.port);
-});
-
+handler.on('issues', function (event) {
+  console.log('Received an issue event for %s action=%s: #%d %s',
+    event.payload.repository.name,
+    event.payload.action,
+    event.payload.issue.number,
+    event.payload.issue.title)
+})
